bea01a1b8034f57b2322207f731127d42ae243ab
riddhima44 <riddhimab19@gmail.com>
2026-01-19 21:57:57 +0530

    Create 3351-MaximizeHappinessOfSelectedChildren

---
+++ 3351-MaximizeHappinessOfSelectedChildren/3351-MaximizeHappinessOfSelectedChildren.java
@@ -0,0 +1,17 @@
+// Last updated: 1/19/2026, 9:57:46 PM
+import java.util.*;
+
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness); // ascending
+        long sum = 0;
+
+        int n = happiness.length;
+        for (int i = 0; i < k; i++) {
+            int val = happiness[n - 1 - i] - i;
+            if (val <= 0) break;
+            sum += val;
+        }
+        return sum;
+    }
+}
