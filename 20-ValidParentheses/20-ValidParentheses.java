ca60128c64194e23a4a1c96dab52cfc22020aefc
riddhima44 <riddhimab19@gmail.com>
2026-01-19 22:50:42 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,20 @@
+// Last updated: 1/19/2026, 10:50:30 PM
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) return false;
+                char top = st.pop();
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
