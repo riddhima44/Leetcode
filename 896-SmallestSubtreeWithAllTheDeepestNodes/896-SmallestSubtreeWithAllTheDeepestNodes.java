bbaf5b3284b0aa933c1addf1c985fb97957d6ff3
riddhima44 <riddhimab19@gmail.com>
2026-01-19 22:48:40 +0530

    Create 896-SmallestSubtreeWithAllTheDeepestNodes

---
+++ 896-SmallestSubtreeWithAllTheDeepestNodes/896-SmallestSubtreeWithAllTheDeepestNodes.java
@@ -0,0 +1,51 @@
+// Last updated: 1/19/2026, 10:48:28 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(null, 0);
+        }
+
+        Result left = dfs(root.left);
+        Result right = dfs(root.right);
+
+        if (left.depth > right.depth) {
+            return new Result(left.node, left.depth + 1);
+        }
+        if (right.depth > left.depth) {
+            return new Result(right.node, right.depth + 1);
+        }
+
+        // depths equal → current node is LCA
+        return new Result(root, left.depth + 1);
+    }
+
+    static class Result {
+        TreeNode node;
+        int depth;
+
+        Result(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+}
