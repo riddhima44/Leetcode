3c6f13352a977c7d2c523e476821e5401add5e35
riddhima44 <riddhimab19@gmail.com>
2026-01-19 22:49:46 +0530

    Create 153-FindMinimumInRotatedSortedArray

---
+++ 153-FindMinimumInRotatedSortedArray/153-FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,51 @@
+// Last updated: 1/19/2026, 10:49:35 PM
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int start=0;
+//         int end=nums.length-1;
+//         int min =Integer.MAX_VALUE;
+//         while(start<=end){
+//             int mid=start + (end-start)/2;
+//             //left sorted array
+//             if(nums[mid]>=nums[start]){
+//                 min = Math.min(min,nums[start]);
+//                 start=mid+1;
+//             }
+//             //right sorted array
+//             else if(nums[end]>=nums[mid]){
+//                 min = Math.min(min,nums[mid]);
+//                 end=mid-1;
+//             }
+//         }
+//         return min;
+//     }
+// }
+
+
+class Solution {
+    public int findMin(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        int min =Integer.MAX_VALUE;
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            //if the entire search space is sorted 
+            //then arr[low] will be smaller among them
+            if(nums[start]<=nums[end]){
+                min = Math.min(min,nums[start]);
+                break;
+            }
+            //left sorted array
+            if(nums[mid]>=nums[start]){
+                min = Math.min(min,nums[start]);
+                start=mid+1;
+            }
+            //right sorted array
+            else if(nums[end]>=nums[mid]){
+                min = Math.min(min,nums[mid]);
+                end=mid-1;
+            }
+        }
+        return min;
+    }
+}
