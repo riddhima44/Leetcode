6eba3c4bc2f7a9f95ad837d948dd735366daea95
riddhima44 <riddhimab19@gmail.com>
2026-01-19 22:47:25 +0530

    Create 1395-MinimumTimeVisitingAllPoints

---
+++ 1395-MinimumTimeVisitingAllPoints/1395-MinimumTimeVisitingAllPoints.java
@@ -0,0 +1,14 @@
+// Last updated: 1/19/2026, 10:47:13 PM
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        
+        for (int i = 1; i < points.length; i++) {
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
+            time += Math.max(dx, dy);
+        }
+        
+        return time;
+    }
+}
