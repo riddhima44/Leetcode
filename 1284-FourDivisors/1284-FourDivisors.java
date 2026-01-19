f9f26a3463ac185fc625d62f0f7e868f87fd4457
riddhima44 <riddhimab19@gmail.com>
2026-01-19 22:47:33 +0530

    Create 1284-FourDivisors

---
+++ 1284-FourDivisors/1284-FourDivisors.java
@@ -0,0 +1,34 @@
+// Last updated: 1/19/2026, 10:47:21 PM
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+
+        for (int num : nums) {
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    int d1 = i;
+                    int d2 = num / i;
+
+                    count++;
+                    sum += d1;
+
+                    if (d1 != d2) {
+                        count++;
+                        sum += d2;
+                    }
+
+                    // no need to continue — already invalid
+                    if (count > 4) break;
+                }
+            }
+
+            if (count == 4) {
+                totalSum += sum;
+            }
+        }
+        return totalSum;
+    }
+}
